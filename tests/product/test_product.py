45da4d6ec7a786283e676a1be060b4d673324141
PatriciaDivi <patriciavianad@gmail.com>
2022-10-28 17:59:55 -0300

    feat(TESTS): :sparkles: Implementado Teste de verificação dos paramentros recebidos na class 'Product'

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui....
+    dataModel = Product(
+        1,
+        "Borracha",
+        "Papelaria Solar",
+        "2021-07-04",
+        "2029-02-09",
+        "FR48",
+        "Ao abrigo de luz solar"
+    ) 
+    
+    assert dataModel.id == 1
+    assert dataModel.nome_do_produto == "Borracha"
+    assert dataModel.nome_da_empresa == "Papelaria Solar"
+    assert dataModel.data_de_fabricacao == "2021-07-04"
+    assert dataModel.data_de_validade == "2029-02-09"
+    assert dataModel.numero_de_serie == "FR48"
+    assert dataModel.instrucoes_de_armazenamento == "Ao abrigo de luz solar"
