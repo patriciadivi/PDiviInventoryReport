b0a34e4aacd945aa39baf0a73964f980f226ecb3
PatriciaDivi <patriciavianad@gmail.com>
2022-11-03 00:11:53 -0300

    feat(Development): :sparkles: Creating the 'Inventory' class
    Implementing 'openFile' methods for '.csv' file read verification
    
    Implementing 'import_data' methods to show full and simple file

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,24 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
+
+
+class Inventory:
+    @classmethod
+    def openFile(cls, filePath):
+        if filePath.endswith(".csv"):  # endswith pega o fim do paramentro
+            with open(filePath, "r") as file:
+                  inventory = list(csv.DictReader(file))
+                  return inventory
+
+    @classmethod
+    def import_data(cls, filePath, typePath):
+        filePathFinsh = cls.openFile(filePath)
+
+        if typePath == "simples":
+            resFinsh = SimpleReport.generate(filePathFinsh)
+            return resFinsh
+
+        if typePath == "completo":
+            resFinsh = CompleteReport.generate(filePathFinsh)
+            return resFinsh
